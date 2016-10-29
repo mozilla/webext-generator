fe7e0f3128369522aebca52d3516bd670c282f89
Stuart Colville <scolville@mozilla.com>
2016-10-29 17:04:48 +0100

    Very rough initial version

---
+++ js/webextgen.js
@@ -0,0 +1,42 @@
+(function () {
+  var chain = new Foswig(2);
+  chain.addWordsToChain(window.__webextZipNames);
+  function genWord() {
+    return chain.generateWord(5,10,false);
+  }
+  var webExtName =  [genWord(), genWord(), genWord()]
+    .join('-').toLowerCase().replace(' ', '');
+
+
+  var zip = new JSZip();
+  var manifest = {
+    manifest_version: 2,
+    version: '1.0',
+    description: 'A test extension from webext-generator.',
+    icons: {
+      48: 'icons/border-48.png'
+    },
+  };
+
+  manifest.name = webExtName;
+
+  zip.file('manifest.json', JSON.stringify(manifest));
+  var downloadButton = document.getElementById('download');
+  document.getElementById('zipname').innerText = webExtName + '.zip';
+
+  if (JSZip.support.blob) {
+    function downloadWithBlob() {
+      zip.generateAsync({type: 'blob'}).then(function (blob) {
+        saveAs(blob, webExtName + '.zip');
+      }, function (err) {
+        console.error(err);
+      });
+      return false;
+    }
+    downloadButton.addEventListener('click', downloadWithBlob, false);
+  } else {
+    downloadButton.innerText += ' (not supported on this browser)';
+    downloadButton.setAttribute('disabled', true);
+  }
+})();
+

62fc2d03793bbf2830eea376cf14d53644259f17
Stuart Colville <scolville@mozilla.com>
2016-10-31 21:36:48 +0000

    Add refresh button in the page (fixes #4)

--- js/webextgen.js
+++ js/webextgen.js
@@ -1,32 +1,47 @@
 (function () {
-  var chain = new Foswig(2);
-  chain.addWordsToChain(window.__webextZipNames);
-  function genWord() {
-    return chain.generateWord(5,10,false);
+  var webExtName;
+  var webExtZip;
+
+  function generateExtensionName() {
+    var chain = new Foswig(2);
+    chain.addWordsToChain(window.__webextZipNames);
+    function genWord() {
+      return chain.generateWord(5,10,false);
+    }
+    return [genWord(), genWord(), genWord()]
+      .join('-').toLowerCase().replace(' ', '');
   }
-  var webExtName =  [genWord(), genWord(), genWord()]
-    .join('-').toLowerCase().replace(' ', '');
 
+  function buildZip(name) {
+    var zip = new JSZip();
+    var manifest = {
+      manifest_version: 2,
+      version: '1.0',
+      description: 'A test extension from webext-generator.',
+      icons: {
+        48: 'icons/border-48.png'
+      },
+    };
+    manifest.name = webExtName;
+    zip.file('manifest.json', JSON.stringify(manifest, null, 4));
+    return zip;
+  }
 
-  var zip = new JSZip();
-  var manifest = {
-    manifest_version: 2,
-    version: '1.0',
-    description: 'A test extension from webext-generator.',
-    icons: {
-      48: 'icons/border-48.png'
-    },
-  };
+  function generate() {
+    webExtName = generateExtensionName();
+    webExtZip = buildZip(webExtName);
+    document.getElementById('zipname').innerText = webExtName + '.zip';
+  }
 
-  manifest.name = webExtName;
+  generate();
 
-  zip.file('manifest.json', JSON.stringify(manifest, null, 4));
+  var refreshButton = document.getElementById('refresh');
   var downloadButton = document.getElementById('download');
-  document.getElementById('zipname').innerText = webExtName + '.zip';
+  refreshButton.addEventListener('click', generate, false);
 
   if (JSZip.support.blob) {
     function downloadWithBlob() {
-      zip.generateAsync({type: 'blob'}).then(function (blob) {
+      webExtZip.generateAsync({type: 'blob'}).then(function (blob) {
         saveAs(blob, webExtName + '.zip');
       }, function (err) {
         console.error(err);
